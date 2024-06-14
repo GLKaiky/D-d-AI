b9b8c93c40a99ac671b076ebceab2dae00ee5729
GLKaiky <kaikyfranca8@gmail.com>
2024-06-14 18:01:36 -0300

    Added Flask Server and Main AI

--- main.py
+++ main.py
@@ -21,37 +21,6 @@ def recomendar():
 
     return jsonify({'recomendacoes': recomendacoes})
 
-@app.route('/analisar', methods = ['POST'])
-def analisar():
-
-    data = request.get_json()
-    image_data = data['image']
-    image_bytes = base64.b64decode(image_data)
-
-    # URL do endpoint de análise de imagem do Azure Computer Vision
-    url = "https://iadideas.cognitiveservices.azure.com/vision/v3.0/analyze"
-
-    # Chave de assinatura do Azure Computer Vision
-    subscription_key = "64c15232e0094dbd8bf81a6730dd0f5f"
-
-    # Parâmetros da solicitação
-    params = {
-        'visualFeatures': 'Categories,Description,Color',
-        'language': 'pt'
-    }
-
-    # Headers da solicitação com a chave de assinatura
-    headers = {
-        'Content-Type': 'application/octet-stream',
-        'Ocp-Apim-Subscription-Key': subscription_key,
-    }
-
-    # Enviar a solicitação POST com a imagem para o serviço do Azure Computer Vision
-    response = requests.post(url, headers=headers, params=params, data=image_bytes)
-    print("sucesso")
-
-    return response.json()
-
 if __name__ == '__main__':
     app.run(debug=True, port=9090)
 
